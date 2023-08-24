afd3e670b5f03f779ed67f59b06ad8d4fd5f727b
Olvine23 <olivinegeorge@gmail.com>
2023-08-24 13:23:20 +0300

    feat: caseload workflow

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 // final prefs = await SharedPreferences.getInstance();
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const CPIMS());
 }
 
--- lib/screens/homepage/home_page.dart
+++ lib/screens/homepage/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cpims_mobile/constants.dart';
 import 'package:cpims_mobile/providers/ui_provider.dart';
 import 'package:cpims_mobile/screens/homepage/widgets/homepage_card.dart';
@@ -30,116 +32,121 @@ class _HomepageState extends State<Homepage> {
       drawer: const Drawer(
         child: CustomDrawer(),
       ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        children: [
-          const SizedBox(height: 20),
-          const Text('4THE CHILD - Dashboard',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black)),
-          const SizedBox(height: 5),
-          const Text(
-            'Application data and usage summary',
-            style: TextStyle(color: kTextGrey),
-          ),
-
-          HomepageCard(
-            data: {
-              'title': 'Org Unit Id',
-              'value': context
-                  .read<UIProvider>()
-                  .getDashData['org_unit_id']
-                  .toString(),
-              'icon': FontAwesomeIcons.orcid,
-              'color': Colors.black54,
-              's_color': const Color(0xff0E6668),
-            },
-            // homeCardsTitles[1],
-            // context.read<UIProvider>().homeCardsTitles[1]
-          ),
-
-          HomepageCard(
-            data: {
-              'title': 'OVC-ACTIVE/EVER REGISTERED',
-              'value': context
-                      .read<UIProvider>()
-                      .getDashData['caregivers']
-                      .toString() ??
-                  "null",
-              'icon': FontAwesomeIcons.person,
-              'color': kPrimaryColor,
-              's_color': const Color(0xff0E6668),
-            },
-            // homeCardsTitles[1],
-            // context.read<UIProvider>().homeCardsTitles[1]
-          ),
-
-          HomepageCard(
-            data: {
-              'title': 'CAREGIVERS/GUARDIANS',
-              'value': context
-                      .read<UIProvider>()
-                      .getDashData['caregivers']
-                      .toString() ??
-                  "null",
-              'icon': FontAwesomeIcons.peopleGroup,
-              'color': const Color(0xff348FE2),
-              's_color': const Color(0xff1F5788),
-            },
-          ),
-
-          HomepageCard(
-            data: {
-              'title': 'WORKFORCE MEMBERS',
-              'value': context
-                      .read<UIProvider>()
-                      .getDashData['workforce_members']
-                      .toString() ??
-                  "null",
-              'icon': Icons.people,
-              'color': const Color(0xff727DB6),
-              's_color': const Color(0xff454A6D),
-            },
-          ),
-          HomepageCard(
-            data: {
-              'title': 'ORG UNITS/CBOs',
-              'value': context
-                      .read<UIProvider>()
-                      .getDashData['org_units']
-                      .toString() ??
-                  "null",
-              'icon': FontAwesomeIcons.landmark,
-              'color': const Color(0xff49B6D5),
-              's_color': const Color(0xff2C6E80),
-            },
-          ),
-          HomepageCard(data: {
-            'title': 'HOUSEHOLDS',
-            'value':
-                context.read<UIProvider>().getDashData['hh_holds'].toString() ??
+      body: WillPopScope(
+        onWillPop: (){
+          exit(0);
+        },
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          children: [
+            const SizedBox(height: 20),
+            const Text('4THE CHILD - Dashboard',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black)),
+            const SizedBox(height: 5),
+            const Text(
+              'Application data and usage summary',
+              style: TextStyle(color: kTextGrey),
+            ),
+      
+            HomepageCard(
+              data: {
+                'title': 'Org Unit Id',
+                'value': context
+                    .read<UIProvider>()
+                    .getDashData['org_unit_id']
+                    .toString(),
+                'icon': FontAwesomeIcons.orcid,
+                'color': Colors.black54,
+                's_color': const Color(0xff0E6668),
+              },
+              // homeCardsTitles[1],
+              // context.read<UIProvider>().homeCardsTitles[1]
+            ),
+      
+            HomepageCard(
+              data: {
+                'title': 'OVC-ACTIVE/EVER REGISTERED',
+                'value': context
+                        .read<UIProvider>()
+                        .getDashData['caregivers']
+                        .toString() ??
                     "null",
-            'icon': FontAwesomeIcons.house,
-            'color': const Color(0xffFE5C57),
-            's_color': const Color(0xff9A3734),
-          }),
-
-          // const SizedBox(height: 10),
-          // ...List.generate(
-          //   context.read<UIProvider>().homeCardsTitles.length,
-          //   (index) => HomepageCard(
-          //       data: context.read<UIProvider>().homeCardsTitles[index]
-          //       // homeCardsTitles[index],
-          //       ),
-          // ),
-          // ...List.generate(
-          //     5,
-          //     (index) => GraphWidget(
-          //           title: graphTitles[index],
-          //         )),
-        ],
+                'icon': FontAwesomeIcons.person,
+                'color': kPrimaryColor,
+                's_color': const Color(0xff0E6668),
+              },
+              // homeCardsTitles[1],
+              // context.read<UIProvider>().homeCardsTitles[1]
+            ),
+      
+            HomepageCard(
+              data: {
+                'title': 'CAREGIVERS/GUARDIANS',
+                'value': context
+                        .read<UIProvider>()
+                        .getDashData['caregivers']
+                        .toString() ??
+                    "null",
+                'icon': FontAwesomeIcons.peopleGroup,
+                'color': const Color(0xff348FE2),
+                's_color': const Color(0xff1F5788),
+              },
+            ),
+      
+            HomepageCard(
+              data: {
+                'title': 'WORKFORCE MEMBERS',
+                'value': context
+                        .read<UIProvider>()
+                        .getDashData['workforce_members']
+                        .toString() ??
+                    "null",
+                'icon': Icons.people,
+                'color': const Color(0xff727DB6),
+                's_color': const Color(0xff454A6D),
+              },
+            ),
+            HomepageCard(
+              data: {
+                'title': 'ORG UNITS/CBOs',
+                'value': context
+                        .read<UIProvider>()
+                        .getDashData['org_units']
+                        .toString() ??
+                    "null",
+                'icon': FontAwesomeIcons.landmark,
+                'color': const Color(0xff49B6D5),
+                's_color': const Color(0xff2C6E80),
+              },
+            ),
+            HomepageCard(data: {
+              'title': 'HOUSEHOLDS',
+              'value':
+                  context.read<UIProvider>().getDashData['hh_holds'].toString() ??
+                      "null",
+              'icon': FontAwesomeIcons.house,
+              'color': const Color(0xffFE5C57),
+              's_color': const Color(0xff9A3734),
+            }),
+      
+            // const SizedBox(height: 10),
+            // ...List.generate(
+            //   context.read<UIProvider>().homeCardsTitles.length,
+            //   (index) => HomepageCard(
+            //       data: context.read<UIProvider>().homeCardsTitles[index]
+            //       // homeCardsTitles[index],
+            //       ),
+            // ),
+            // ...List.generate(
+            //     5,
+            //     (index) => GraphWidget(
+            //           title: graphTitles[index],
+            //         )),
+          ],
+        ),
       ),
     );
   }
