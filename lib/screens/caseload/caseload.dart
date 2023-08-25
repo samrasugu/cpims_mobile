bd12d3fdae70bf836ee7ffb98412f5221fbf8faa
Ryan Koech <sirryankoech@gmail.com>
2023-08-25 15:57:14 +0300

    merge master

--- lib/screens/caseload/caseload.dart
+++ lib/screens/caseload/caseload.dart
@@ -1,15 +1,9 @@
-import 'dart:convert';
-
-import 'package:cpims_mobile/Models/case_load.dart';
 import 'package:cpims_mobile/providers/db_provider.dart';
-import 'package:cpims_mobile/providers/ui_provider.dart';
 
-import 'package:cpims_mobile/services/api_service.dart';
 import 'package:cpims_mobile/services/caseload_service.dart';
 import 'package:cpims_mobile/widgets/app_bar.dart';
 import 'package:cpims_mobile/widgets/drawer.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class CaseLoad extends StatefulWidget {
   const CaseLoad({Key? key}) : super(key: key);
@@ -19,18 +13,9 @@ class CaseLoad extends StatefulWidget {
 }
 
 class _CaseLoadState extends State<CaseLoad> {
-  @override
-  void initState() {
-    getData();
-    super.initState();
-  }
 
   final CaseLoadService caseLoadService = CaseLoadService();
 
-  void getData() async {
-    await CaseLoadDb.instance.retrieveCaseLoads();
-    // await caseLoadService.fetchCaseLoadData(context: context);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,28 +40,8 @@ class _CaseLoadState extends State<CaseLoad> {
           return ListView.builder(
             itemCount: caseLoadData!.length,
             itemBuilder: (BuildContext context, int index) {
-              return  Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        "Cpims Id:  ${caseLoadData[index].cpimsId}  "),
-                         Text(
-                        "First Name:  ${caseLoadData[index].ovc_first_name}"),
-                         Text(
-                        "Last Name: ${caseLoadData[index].ovc_surname}"),
-                         Text(
-                        "Caregiver names: ${caseLoadData[index].caregiver_names}"),
-                         Text(
-                        "Date Of Birth:  ${caseLoadData[index].date_of_birth}"),
-                         Text(
-                        "Registration Date: ${caseLoadData[index].registration_date}"),
-                         Text(
-                        "Sex:  ${caseLoadData[index].sex}"),
-                        
-                  ],
-                ),
-              );
+              return Text(
+                  "${caseLoadData[index].cpimsId} ${caseLoadData[index].ovc_first_name}");
             },
           );
         },
